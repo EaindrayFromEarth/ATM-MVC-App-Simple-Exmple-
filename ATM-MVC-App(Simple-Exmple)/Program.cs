5bce94a2c76b6970c7d0b9418d1edb7d25951544
Eaindray <eaindrayfromearth@gmail.com>
2024-01-04 20:07:30 +0630

    session config

--- ATM-MVC-App(Simple-Exmple)/Program.cs
+++ ATM-MVC-App(Simple-Exmple)/Program.cs
@@ -7,6 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); 
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 
 builder.Services.AddDbContext<AtmContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -21,6 +30,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
